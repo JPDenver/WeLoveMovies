a9a78db930ddfd137fecf2e082acc39422842994
Jeff <jeffrey.paul.den@gmail.com>
2021-07-12 11:54:31 -0600

    first commit

---
+++ src/db/migrations/20210711130628_updateMovieDesc.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.table("movies", (table) => {
+        table.text("description");
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.table("movies", (table) => {
+        table.dropColumn("description");
+      });
+};
